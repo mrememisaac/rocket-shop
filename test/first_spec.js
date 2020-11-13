6d59810c528695359ddb2769b1a2df0f08e1bb1b
Emem Isaac <me@ememisaac.com>
2020-11-13 15:48:15 +0100

    Initial load

---
+++ test/first_spec.js
@@ -0,0 +1,8 @@
+const assert = require("assert");
+describe("A feature", function(){
+  describe("A scenario", function(){
+    it("does something expected", function(){
+      assert(true);
+    })
+  })
+})
